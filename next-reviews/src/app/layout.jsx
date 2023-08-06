3e983ec4d8d64466609fe12c75e4b8a11899f1fe
MLDODSON <paola@dominicansoul.com>
2023-08-06 10:14:08 -0500

    Branch 8
    Imported fonts and customized them in Tailwind.

--- next-reviews/src/app/layout.jsx
+++ next-reviews/src/app/layout.jsx
@@ -1,9 +1,11 @@
 import NavBar from '../components/NavBar'
+import { orbitron } from './fonts';
+import { exoTwo } from './fonts';
 import './globals.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
+    <html lang='en' className={`${exoTwo.variable} ${orbitron.variable}`}>
       <body className='bg-orange-50 flex flex-col px-4 py-2 min-h-screen'>
         <header>
           <NavBar />
@@ -11,7 +13,7 @@ export default function RootLayout({ children }) {
         <main className='grow py-3'>
           {children}
         </main>
-        <footer className='border-t py-3 text-center text-xs '>
+        <footer className='border-t py-3 text-center text-xs font-exoTwo'>
           Game data and images courtesy of <a href='https://rawg.io/' target='_blank' className='text-orange-800 hover:underline font-semibold'>RAWG</a>
         </footer>
       </body>
