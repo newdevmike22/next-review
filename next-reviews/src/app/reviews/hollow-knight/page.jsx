3ff79e0749b85302fd9a2192a0c6aa0865711ee5
MLDODSON <paola@dominicansoul.com>
2023-08-06 09:04:11 -0500

    Branch 6
    Added images to the reviews pages.

--- next-reviews/src/app/reviews/hollow-knight/page.jsx
+++ next-reviews/src/app/reviews/hollow-knight/page.jsx
@@ -4,6 +4,9 @@ export default function HollowKnightPage() {
     return (
       <>
         <Heading>Hollow Knight</Heading>
+        <img src="/images/hollow-knight.jpg" alt="hollow knight" 
+             width="640" height="360" className="mb-2 rounded" 
+        />
         <p>
           Hollow Knight review.
         </p>
--- next-reviews/src/app/reviews/stardew-valley/page.jsx
+++ next-reviews/src/app/reviews/stardew-valley/page.jsx
@@ -4,6 +4,9 @@ export default function StardewValleyPage() {
     return (
       <>
         <Heading>Stardew Valley</Heading>
+        <img src="/images/stardew-valley.jpg" alt="stardew valley" 
+               width="640" height="360" className="mb-2 rounded" 
+        />
         <p>
           Stardew Valley review.
         </p>
